23e3be8ec0154c92b39667ac1b1f94ebb8290f3c
Mak3r <Mak3r@Mak3r-PC>
2013-03-14 23:10:14 +0000

    multiplicaçao adicionada #2

--- Despesas/src/despesas/Despesas.java
+++ Despesas/src/despesas/Despesas.java
@@ -14,11 +14,15 @@ public class Despesas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        int a=2, b=3;
+        int multi = mult(a,b);
     }
     
     public static int soma(int a, int b){
         return a+b;
     }
     
+    public static int mult(int a, int b){
+        return a*b;
+    }
 }
