2d59d6c19f63caca9ef304f616d6b085ceb72292
Márcio <Mrcio@marcioport>
2013-03-15 19:47:27 +0000

    Projeto git

--- Despesas/src/despesas/Despesa.java
+++ Despesas/src/despesas/Despesa.java
@@ -9,5 +9,44 @@ package despesas;
  * @author Márcio
  */
 public class Despesa {
+    //Atributos
+    private float valor;
+    private String descricao;
+    
+    //Construtores
+    Despesa(float valor,String desString)
+    {
+      this.valor = valor;
+      descricao = desString;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
     
 }
